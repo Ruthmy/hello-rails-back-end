eab6a9087f8a7e2c5bc3557a49ccf8472e6d60a7
Ruthmy <ruthmy17@gmail.com>
2023-11-10 21:33:10 -0600

    Modified 'routes.rb' -file- - Updated the avaible routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'greetings/api'
+  # get 'greetings/api'
+
+  namespace :api do
+    namespace :v1 do
+      resources :greetings, only: [:index]
+    end
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
