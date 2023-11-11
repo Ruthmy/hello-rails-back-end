ebb5ce6a9d74af0964385b679c27c08b27101dc3
Ruthmy <ruthmy17@gmail.com>
2023-11-10 21:25:09 -0600

    Modified 'greetings_controller.rb' -controller- - Change folder/ubication. - Change the class

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,5 +1,5 @@
-class GreetingsController < ApplicationController
-  def api
+class Api::V1::GreetingsController < ApplicationController
+  def index
     random_greeting = Greeting.order('RANDOM()').first
     render json: { message: random_greeting&.message || 'No greetings found' }
   end
